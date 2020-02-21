16f2e47befe8e1f5c586c2a589bc458de61e3883
Danilo Abreu <daniloabreu1@gmail.com>
2020-02-21 01:32:10 -0300

    basico

---
+++ exercicios-JS/fundamentos/funcoes.js
@@ -0,0 +1,14 @@
+function somaValores(a,b){
+    return a+b
+}
+console.log(somaValores(2,4))
+
+//funcao anônima
+const soma = function(a,b){
+    return a+b
+}
+console.log(soma(4,78))
+
+//funcao arrow
+const subtracao =(a,b) => a-b
+console.log(subtracao(100,7))
--- exercicios-JS/fundamentos/organizacao.js
+++ exercicios-JS/fundamentos/organizacao.js
@@ -1,1 +1,10 @@
 console.log("Sentença de texto")
+let n = 3
+console.log(n)
+console.log(typeof n)
+n= 'bom dia'
+console.log(n)
+console.log(typeof n)
+console.log(Number.isInteger(n))
+const fun1 = string1 => string1.toUpperCase()
+console.log(`Ei, tenha ${fun1('cuidado')}`) 
