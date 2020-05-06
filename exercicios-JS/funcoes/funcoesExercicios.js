77a8ffc204224f9586c25932642072f3088ce6fd
Danilo Abreu <daniloabreu1@gmail.com>
2020-05-06 14:53:42 -0300

    funcoes2

--- exercicios-JS/funcoes/funcoesExercicios.js
+++ exercicios-JS/funcoes/funcoesExercicios.js
@@ -5,9 +5,11 @@ function soma() {
     }
     return soma
 }
+function somaPad(a=0,b=0,c=0){
+    return a+b+c
+}
+
 console.log(soma())
 console.log(soma(1))
 console.log(soma(1.1, 2.2, 3.3))
-
-console.log(soma(1.1, 2.2, "Teste"))
-console.log(soma('a', 'b', 'c'))
+console.log(somaPad(),somaPad(1),somaPad(2,3,4))
